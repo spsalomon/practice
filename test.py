7feb8dfa13f19998c52821b1b3e190d0451b659b
simon salomon <ssalomon@gmail.com>
2017-12-23 10:51:18 -0800

    second commit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("This is a test. This should cause a conflict. newfix")
+print("This is a test. This should cause a conflict. newfix. after new fix this
+      is second commit.")
